0c2fe811f053ac27bb1f7adeec174ef526a167c3
Javier Gil <javier@gilpereda.com>
2018-01-03 20:45:07 +0100

    Created the Spring Boot project for the api gateway and configured the docker image

---
+++ api-gateway/src/main/java/com/gilpereda/bddpactmicroservices/apigateway/ApiGateway.java
@@ -0,0 +1,13 @@
+package com.gilpereda.bddpactmicroservices.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateway.class, args);
+    }
+    
+}
