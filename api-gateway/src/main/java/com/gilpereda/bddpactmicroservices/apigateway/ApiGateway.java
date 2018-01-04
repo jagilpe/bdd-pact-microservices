6deb5c3a32cdd0532014f309b17e31cce2285a03
Javier Gil Pereda <javier@gilpereda.com>
2018-01-04 09:58:20 +0100

    Added Netflix Zuul to Api Gateway and configured default mapping for angular frontend

--- api-gateway/src/main/java/com/gilpereda/bddpactmicroservices/apigateway/ApiGateway.java
+++ api-gateway/src/main/java/com/gilpereda/bddpactmicroservices/apigateway/ApiGateway.java
@@ -2,12 +2,14 @@ package com.gilpereda.bddpactmicroservices.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ApiGateway {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGateway.class, args);
     }
-    
+
 }
