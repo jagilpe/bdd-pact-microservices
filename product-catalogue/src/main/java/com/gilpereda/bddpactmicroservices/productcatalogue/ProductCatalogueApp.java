e8119b6eed53ff1453c604893e03466810ce6fee
Javier Gil <javier@gilpereda.com>
2018-01-04 21:10:43 +0100

    Created the Spring Project for the Product Catalogue Service

---
+++ product-catalogue/src/main/java/com/gilpereda/bddpactmicroservices/productcatalogue/ProductCatalogueApp.java
@@ -0,0 +1,13 @@
+package com.gilpereda.bddpactmicroservices.productcatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogueApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatalogueApp.class, args);
+    }
+
+}
