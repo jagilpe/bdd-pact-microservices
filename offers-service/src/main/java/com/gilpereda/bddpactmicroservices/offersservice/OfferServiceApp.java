056e8039dbeb061ffc86babc39304a57abfcfa23
Javier Gil <javier@gilpereda.com>
2018-01-07 17:09:46 +0100

    Created the offers service project

---
+++ offers-service/src/main/java/com/gilpereda/bddpactmicroservices/offersservice/OfferServiceApp.java
@@ -0,0 +1,13 @@
+package com.gilpereda.bddpactmicroservices.offersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfferServiceApp.class, args);
+    }
+
+}
---
+++ product-catalogue/src/main/resources/sql/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO category (name) VALUES
+  ('Category 1'),
+  ('Category 2'),
+  ('Category 3');
+
+INSERT INTO product (name, manufacturer, category_id) VALUES
+  ('Product 1', 'Manufacturer 1', 1),
+  ('Product 2', 'Manufacturer 1', 1),
+  ('Product 3', 'Manufacturer 2', 1),
+  ('Product 4', 'Manufacturer 3', 1),
+  ('Product 5', 'Manufacturer 3', 1),
+  ('Product 6', 'Manufacturer 1', 2),
+  ('Product 7', 'Manufacturer 2', 2),
+  ('Product 8', 'Manufacturer 3', 2),
+  ('Product 9', 'Manufacturer 3', 2),
+  ('Product 10', 'Manufacturer 4', 3),
+  ('Product 11', 'Manufacturer 4', 3);
