0d7f1cd7c64a04938587b7ec18682b0be7d28591
Javier Gil <javier@gilpereda.com>
2018-01-07 19:08:57 +0100

    Added the offers service to the docker compose configuration

---
+++ offers-service/src/main/resources/sql/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO offer (product_id, shop_id, shop_name, price) VALUES
+  (1, 1, 'Example shop 1', 700),
+  (1, 2, 'Example shop 2', 740),
+  (1, 3, 'Example shop 3', 690),
+  (1, 4, 'Example shop 4', 685),
+  (2, 2, 'Example shop 2', 100),
+  (2, 3, 'Example shop 3', 125),
+  (2, 4, 'Example shop 4', 95);
