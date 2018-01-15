6e9ce1d0ccbca8ca011e89f1b119d23f9711551c
Javier Gil Pereda <javier@gilpereda.com>
2018-01-15 13:32:56 +0100

    Added Cors filter for development profile

---
+++ api-gateway/src/main/java/com/gilpereda/bddpactmicroservices/apigateway/ApiGatewayCorsConfiguration.java
@@ -0,0 +1,32 @@
+package com.gilpereda.bddpactmicroservices.apigateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@Profile("devel")
+public class ApiGatewayCorsConfiguration {
+
+    @Value("${bdd-pact.ng-frontend.base-url}")
+    private String frontendBaseUrl;
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(frontendBaseUrl);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
